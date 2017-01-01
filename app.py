8664601eb2a3a9217121ab165c63bb7249cc7baa
Zul Dharmawan <zulfikar.dharmawan@philips.com>
2017-01-01 20:58:50 +0100

    Added specific thing for Stripe

--- app.py
+++ app.py
@@ -10,3 +10,28 @@ stripe_keys = {
 stripe.api_key = stripe_keys['secret_key']
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html', key=stripe_keys['publishable_key'])
+
+@app.route('/charge', methods=['POST'])
+def charge():
+  amount = 500
+
+    customer = stripe.Customer.create(
+        email='customer@example.com',
+        card=request.form['stripeToken']
+    )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='usd',
+        description='Flask Charge'
+    )
+
+  return render_template('charge.html', amount=amount)
+
+if __name__ == '__main__':
+  app.run(debug=True)
